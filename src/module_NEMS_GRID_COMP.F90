948d4577346926101fbd9c69b6f1e3ea3ab4cce5
Dusan Jovic <dusan.jovic@noaa.gov>
2019-12-27 15:43:25 +0000

    Set default values for model_configure variables
     RUN_CONTINUE = .false.
     ENS_SPS = .false.
     TOTAL_MEMBER = 1

--- src/module_NEMS_GRID_COMP.F90
+++ src/module_NEMS_GRID_COMP.F90
@@ -313,6 +313,7 @@
                                   ,value  = ENS_SPS                     &  !<-- Value of control flag for 
                                                                            !    stochastic perturbation scheme
                                   ,label  = 'ENS_SPS:'                  &  !<-- Flag's label in configure file
+                                  ,default= .false.                     &
                                   ,rc     = RC)
       ESMF_ERR_RETURN(RC,RC_INIT)
 !
@@ -368,6 +369,7 @@
       CALL ESMF_ConfigGetAttribute(config = CF_NEMS                     &  !<-- The NEMS configure object
                                   ,value  = TOTAL_MEMBER                &  !<-- Total # of ensemble members
                                   ,label  = 'total_member:'             &  !<-- Flag in configure file 
+                                  ,default= 1                           &
                                   ,rc     = RC)
       ESMF_ERR_RETURN(RC,RC_INIT)
 !
