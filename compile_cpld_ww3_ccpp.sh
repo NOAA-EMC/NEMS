5742a222ad3e62bfebe4f51e0b79961d8d0c593b
Shrinivas.Moorthi <Shrinivas.Moorthi@noaa.gov>
2020-01-16 16:03:22 +0000

    adding a new compile script

---
+++ compile_cpld_ww3_ccpp.sh
@@ -0,0 +1,11 @@
+#make app=coupledFV3_MOM6_CICE_WW3_CCPP      distclean
+ make app=coupledFV3_MOM6_CICE_WW3_CCPP -j 8 build
+
+#
+##############################################################
+# This builds the coupled model - app version
+# Not a 32-bit build, may need to change later for bit-reproducibility checks
+#./NEMS/NEMSAppBuilder rebuild app=coupledFV3_MOM6_CICE
+#cd ./NEMS/exe
+#mv NEMS.x nems_fv3_mom6_cice5.x
+##############################################################
